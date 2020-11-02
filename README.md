7663d04d2ca5960ed0142a686471fe196b0aa344
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-11-01 20:15:28 -0500

    BE set up. Can Get Trouble with Post.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Sk8M8
+
+* Title - Text
+* Description - Text
+* Comments - Text
+* Rating - scale of 1-10
+* Image - Text - Url
+* Address - Text
+* First Visit - DateTime
+* Last Visit - DateTime
+* Latitude - Number
+* Longitude - Number
+* Created At - DateTime
+* Update At - DateTime
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Sk8-M8!',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`litening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`);
 });
